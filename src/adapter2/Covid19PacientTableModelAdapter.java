669e0f49bb558182d7fa55b75794bb94e0db9ed3
ehu <ehu@158.227.126.5>
2025-10-24 18:04:36 +0200

    Ejercicio adapter

--- src/adapter2/Covid19PacientTableModelAdapter.java
+++ src/adapter2/Covid19PacientTableModelAdapter.java
@@ -1,8 +1,12 @@
 package adapter2;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 import javax.swing.table.AbstractTableModel;
 
 import domain.Covid19Pacient;
+import domain.Symptom;
 
 public class Covid19PacientTableModelAdapter extends AbstractTableModel {
 	  protected Covid19Pacient pacient;
@@ -15,21 +19,26 @@ public class Covid19PacientTableModelAdapter extends AbstractTableModel {
 
 	  public int getColumnCount() {
 	    // Challenge!
-		 return 1;
+		 return 2;
 	  }
 
 	  public String getColumnName(int i) {
 	    // Challenge!
-		  return "Column name 1";
+		  return columnNames[i];
 	  }
 
 	  public int getRowCount() {
 	    // Challenge!
-		  return 1;
+		  return pacient.getSymptoms().size();
 	  }
 
 	  public Object getValueAt(int row, int col) {
-	    // Challenge!
-		  return "value";
+
+		  Set<Symptom> losSintomas = pacient.getSymptoms();
+		  Object[] symptoms =losSintomas.toArray();
+		  Symptom s= (Symptom) symptoms[row];
+		  if(col==0)  return s.getName();
+		  if(col ==1) return pacient.getWeight(s);
+		  return null;
 	  }
 	}
